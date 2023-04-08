70f1c38a65e85925eb671bc06bbf52a0953f20d8
Kevin Auerbach <auerbachkevin123@gmail.com>
2023-04-08 17:03:05 -0300

    primera version de navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,82 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <div>
-          <img className="logotipo" src="../../public/images/tufudlogo.png" alt="tufud"/>
-        </div>
-        <div className='row justify-content-end'>
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Servicios
-                </a>
-                <ul className="dropdown-menu dropdown-menu-custom fade-in">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Comunidad
-                </a>
-                <ul className="dropdown-menu dropdown-menu-custom fade-in">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Contacto</a>
-              </li>
-            </ul>
-            <button type="button" class="btn btn-outline-primary">Iniciar sesión</button>
-          </div>
+        <a className="navbar-brand" href="#">
+          <img src="../images/tufudlogo.png" width="150" alt="Tufud Logo" />
+        </a>
+        <button
+          className="navbar-toggler hamburguesa"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse flew-grow-0" id="navbarTogglerDemo02">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Servicios
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Cliente
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Proveedor
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Comunidad
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Testimonios
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Noticias
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#">
+                Contacto
+              </a>
+            </li>
+            <button className="btn fw-semibold">Iniciar Sesion</button>
+          </ul>
         </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
