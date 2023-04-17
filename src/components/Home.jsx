54db37d071a4d58bb6025d969fcc4a30598de4d1
Kevin Auerbach <auerbachkevin123@gmail.com>
2023-04-17 18:33:52 -0300

    cambio en el home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ import BtnSubir from "./BtnSubir";
 
 const Home = () => {
     return (
-        <div className="App">
+        <div>
             <Info/>
             <Beneficios/>
             <Info2/>
