5e5666feef6726d45b7ea3e25ad8f50c835b5b46
patricioriel <patricio.riel@hotmail.com>
2023-04-18 15:39:20 -0300

    modifico responsive de botones res y prov

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -8,8 +8,8 @@ function Info() {
         <div>
           <h1 className='fw-bold text-start display-3'>Somos la plataforma de pedidos que revoluciona la industria gastronómica.</h1>
           <p className='text-start fs-4'>Tufud ayuda a restaurantes y proveedores a mejorar el envío y recepción de ordenes de compra, con el objetivo de ahorrar tiempo, disminuir costos y reducir el desperdicio de alimentos.</p>
-          <Link to={"/restaurante"} type="button" className="btn btn-primary me-4 fs-4 btninfo btn-zoom">Restaurante</Link>
-          <Link type="button" className="btn btn-primary fs-4 btninfo btn-zoom">Proveedor</Link>
+          <Link to={"/restaurante"} className="btn my-4 fw-semibold fs-2 ps-5 pe-5 btn-zoom me-5">Restaurante</Link>
+          <Link className="btn my-4 fw-semibold fs-2 ps-5 pe-5 btn-zoom me-5">Proveedor</Link>
         </div>
       </div>
       <div className="col-md-5">
