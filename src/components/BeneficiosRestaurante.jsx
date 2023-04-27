a69a9f63f6ec8a4a35ab5634fe2226fc8e052b94
patricioriel <patricio.riel@hotmail.com>
2023-04-27 18:03:29 -0300

    agrego imagenes

--- src/components/BeneficiosRestaurante.jsx
+++ src/components/BeneficiosRestaurante.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+
 function BeneficiosRestaurante() {
     return (
         <div class="container-fluid benef">
--- src/components/Iniciarsesion.jsx
+++ src/components/Iniciarsesion.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import imgproveedor from "../images/imgproveedor.jpg"
+import imgrestaurante from "../images/imgrestaurante.jpg"
 
 const Iniciarsesion = () => {
   return (
@@ -7,7 +9,7 @@ const Iniciarsesion = () => {
       <div className="row">
         <div className="col-md-6">
           <div className="card w-75">
-            <img src="https://w7.pngwing.com/pngs/115/901/png-transparent-question-mark-graphy-small-plastic-people-people-plastic-bottle-cartoon.png" className="card-img-top img-fluid" alt="..." />
+            <img src={imgrestaurante} className="card-img-top img-fluid" alt="..." />
             <div className="card-body">
               <Link to={"#"} className="btn my-4 fw-semibold fs-2 ps-5 pe-5 btn-zoom me-5">Soy Restaurante</Link>
             </div>
@@ -15,7 +17,7 @@ const Iniciarsesion = () => {
         </div>
         <div className="col-md-6">
           <div class="card w-75">
-            <img src="https://w7.pngwing.com/pngs/115/901/png-transparent-question-mark-graphy-small-plastic-people-people-plastic-bottle-cartoon.png" className="card-img-top img-fluid" alt="..." />
+            <img src={imgproveedor} className="card-img-top img-fluid" alt="..." />
             <div className="card-body">
               <Link to={"#"} className="btn my-4 fw-semibold fs-2 ps-5 pe-5 btn-zoom me-5">Soy Proveedor</Link>
             </div>
