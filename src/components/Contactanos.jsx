7371d79e55bfba219b27bab1c438f72f80b65859
patricioriel <patricio.riel@hotmail.com>
2023-04-28 11:17:19 -0300

    logo anii

--- src/components/Contactanos.jsx
+++ src/components/Contactanos.jsx
@@ -7,10 +7,10 @@ const Contactanos = () => {
         <div className="container-fluid gradiente_verde">
             <div className="container p-5">
                 <div className="row">
-                    <div className="col-md-6">
-                        <h3 className="text-start display-3 fw-bold quicksand"><span className="morado">Mejora la gestión</span> de tus pedidos hoy mismo</h3>
-                        <p className="my-3 text-start fs-3 ">Es hora de cambiar tu enfoque en la gestión de pedidos. Nuestro equipo de expertos esta siempre disponible para ayudarte a hacerlo de manera rápida y fácil. Contáctanos ahora mismo.</p>
-                        <Link className="btn my-4 fw-semibold fs-5" to="">Contáctanos</Link>
+                    <div className="col-md-6 text-start">
+                        <h3 className="text-start titulo fw-bold quicksand"><span className="morado">Mejora la gestión</span> de tus pedidos hoy mismo</h3>
+                        <p className="my-3 text-start cuerpo ">Es hora de cambiar tu enfoque en la gestión de pedidos. Nuestro equipo de expertos esta siempre disponible para ayudarte a hacerlo de manera rápida y fácil. Contáctanos ahora mismo.</p>
+                        <Link className="btn my-4 cuerpofooter" to="/restaurante">Contáctanos</Link>
                     </div>
                     <div className="col-md-6">
                         <img className="img-fluid" src={img3home} width="800" alt="" />
