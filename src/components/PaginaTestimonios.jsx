6a5bd6cd2cdc8fb05105b9de5311a8cefa23f691
patricioriel <patricio.riel@hotmail.com>
2023-05-25 20:22:31 -0300

    responsive testimonios

--- src/components/PaginaTestimonios.jsx
+++ src/components/PaginaTestimonios.jsx
@@ -43,7 +43,7 @@ const PaginaTestimonios = () => {
             <img className="img5" src={picolina} width="60" alt="" />
             <img className="img6" src={tienda} width="60" alt="" />
             <img className="img7" src={mercado} width="60" alt="" />
-            <Link to={"#"} className="btn">
+            <Link to={"#"} className="btn btntest">
               Comienza ahora
             </Link>
             <p className="text-center cuerpocard_testimonios mt-1">
@@ -54,7 +54,7 @@ const PaginaTestimonios = () => {
 
         {/* testimonio 1 */}
 
-        <div className="row my-5">
+        <div className="row my-5 containertest">
           <div className="col-md-7 caja_testimonio order-2 order-md-1">
             <div className="d-flex">
               <img src={comillas} alt="" className="me-1 comillas" />
@@ -68,7 +68,7 @@ const PaginaTestimonios = () => {
               <p className="cuerpo_testimonios text-start me-5">
                 {testimonios[0].descripcion}
               </p>
-              <p className="text-start titulos_testimonios mb-0 mt-5">
+              <p className="text-start titulos_testimonios personatest mb-0 mt-5">
                 {testimonios[0].persona}
               </p>
               <p className="text-start quicksand puesto_testimonios">
@@ -85,14 +85,14 @@ const PaginaTestimonios = () => {
             <img className="fondo_logos_testimonios" src={saludable} alt="" />
           </div>
           <Link
-            className="morado conocelos quicksand fw-bold text-center my-5"
+            className="morado conocelos quicksand fw-bold text-center my-5 order-3 order-md-3"
             to={`#`}
           >
             Conocelos
             <FontAwesomeIcon
               icon={faArrowRight}
               size="m"
-              className="ms-3"
+              className="ms-3 flechatest"
               style={{ color: "#7858dd" }}
             />
           </Link>
@@ -100,7 +100,7 @@ const PaginaTestimonios = () => {
 
         {/* testimonio 2 */}
 
-        <div className="row my-5">
+        <div className="row my-5 containertest">
           <div className="col-md-5 imagen_testimonio2">
             <img
               className="fondo_imagenes_testimonios"
@@ -109,19 +109,13 @@ const PaginaTestimonios = () => {
             />
             <img className="fondo_logos_testimonios" src={tienda} alt="" />
           </div>
-          <div className="col-md-7 cajanoticias">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="80"
-                fill="currentColor"
-                className="bi bi-quote d-flex"
-                viewBox="0 0 16 16"
-              >
-                <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
-              </svg>
+          <div className="col-md-7 caja_testimonio">
+            <div className="d-flex">
+            <img src={comillas} alt="" className="me-1 comillas" />
+              <img src={comillas} alt="" className="comillas" />
             </div>
-            <div className="ms-3">
+            <div className="ms-3 textostest">
+            <img className="logocardtest d-none d-block" src={tienda} alt="" />
               <h2 className="fw-bold titulos_testimonios text-start mb-4 me-5">
                 {testimonios[1].titulo}
               </h2>
@@ -144,7 +138,7 @@ const PaginaTestimonios = () => {
             <FontAwesomeIcon
               icon={faArrowRight}
               size="m"
-              className="ms-3"
+              className="ms-3 flechatest"
               style={{ color: "#7858dd" }}
             />
           </Link>
@@ -152,20 +146,14 @@ const PaginaTestimonios = () => {
 
         {/* testimonio 3 */}
 
-        <div className="row my-5">
-          <div className="col-md-7 caja_testimonio">
-            <div>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="80"
-                fill="currentColor"
-                className="bi bi-quote d-flex"
-                viewBox="0 0 16 16"
-              >
-                <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
-              </svg>
+        <div className="row my-5 containertest">
+          <div className="col-md-7 caja_testimonio order-2 order-md-1">
+            <div className="d-flex">
+            <img src={comillas} alt="" className="me-1 comillas" />
+              <img src={comillas} alt="" className="comillas" />
             </div>
-            <div className="ms-3">
+            <div className="ms-3 textostest">
+            <img className="logocardtest d-none d-block" src={sd} alt="" />
               <h2 className="fw-bold titulos_testimonios text-start mb-4 me-5">
                 {testimonios[2].titulo}
               </h2>
@@ -180,19 +168,19 @@ const PaginaTestimonios = () => {
               </p>
             </div>
           </div>
-          <div className="col-md-5 imagen_testimonio">
+          <div className="col-md-5 imagen_testimonio order-1 order-md-2">
             <img className="fondo_imagenes_testimonios" src={fondosd} alt="" />
             <img className="fondo_logos_testimonios" src={sd} alt="" />
           </div>
           <Link
-            className="morado conocelos quicksand fw-bold text-center my-5"
+            className="morado conocelos quicksand fw-bold text-center my-5 order-3 order-md-3"
             to={`#`}
           >
             Conocelos
             <FontAwesomeIcon
               icon={faArrowRight}
               size="m"
-              className="ms-3"
+              className="ms-3 flechatest"
               style={{ color: "#7858dd" }}
             />
           </Link>
