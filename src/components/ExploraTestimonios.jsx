b9d8e64cc01d5c60cfec3a8172f2b4db7ec312d2
patricioriel <patricio.riel@hotmail.com>
2023-05-27 14:24:56 -0300

    detalles mobile

--- src/components/ExploraTestimonios.jsx
+++ src/components/ExploraTestimonios.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { testimonios } from "../data/DataTestimonios";
+import comillas2 from "../images/comillas2.svg"
 
 function ExploraTestimonios() {
   const [noticiaActual, setNoticiaActual] = useState(0);
@@ -27,17 +28,9 @@ function ExploraTestimonios() {
         <div className="row justify-content-center align-items-center">
           <div className="container_historias row justify-content-center align-items-center">
             <div className="col-md-6 col-sm-12 card_testimonios">
-              <div>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="50"
-                  height="54"
-                  fill="currentColor"
-                  className="icono_testimonio bi bi-quote d-flex"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z" />
-                </svg>
+              <div className="d-flex">
+                <img src={comillas2} alt="" className="comillas2"/>
+                <img src={comillas2} alt="" className="ms-2 comillas2"/>
               </div>
               <h2 className="fw-bold quicksand titulonoticia text-start mb-4 me-5">
                 {testimonios[noticiaActual].titulo}
